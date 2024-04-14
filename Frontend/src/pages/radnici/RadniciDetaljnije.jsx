671506d775a7c0e22470332a339e53be90b4d5bc
Ivan Leninger <ileninger@live.com>
2024-04-14 13:48:32 +0200

    Dodane oznake € u cijenu radnog sata i osoobni odbitak

--- Frontend/src/pages/radnici/RadniciDetaljnije.jsx
+++ Frontend/src/pages/radnici/RadniciDetaljnije.jsx
@@ -7,128 +7,145 @@ import RadnikService from "../../services/RadnikService";
 import { useEffect, useState } from "react";
 
 
-export default function RadniciDetaljnije (){
+export default function RadniciDetaljnije() {
     const navigate = useNavigate();
     const routeParams = useParams();
-    const [radnik,setRadnici] = useState({});
+    const [radnik, setRadnici] = useState({});
 
-    async function dohvatiRadnike(){
+    async function dohvatiRadnike() {
         await RadnikService.getBySifra(routeParams.sifra)
-        .then((res)=>{
-            setRadnici(res.data)
-        })
-        .catch((e)=>{
-            alert(e.poruka);
-        });
+            .then((res) => {
+                setRadnici(res.data)
+            })
+            .catch((e) => {
+                alert(e.poruka);
+            });
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         //console.log("useEffect")
         dohvatiRadnike();
-    },[]);
+    }, []);
 
-    async function promjeniRadnika(radnik){
-        const odgovor = await RadnikService.promjeni(routeParams.sifra,radnik);
-        if(odgovor.ok){
-          navigate(RoutesNames.RADNICI_PREGLED);
-        }else{
-          console.log(odgovor);
-          alert(odgovor.poruka);
+    async function promjeniRadnika(radnik) {
+        const odgovor = await RadnikService.promjeni(routeParams.sifra, radnik);
+        if (odgovor.ok) {
+            navigate(RoutesNames.RADNICI_PREGLED);
+        } else {
+            console.log(odgovor);
+            alert(odgovor.poruka);
         }
     }
 
-    function handleSubmit(e){
+    function handleSubmit(e) {
         e.preventDefault();
         const podaci = new FormData(e.target);
 
-        const radnik = 
+        const radnik =
         {
             ime: podaci.get('ime'),
             prezime: podaci.get('prezime'),
             oib: podaci.get('oib'),
             datumZaposlenja: podaci.get('datumzaposlenja'),
             iban: podaci.get('iban'),
-            cijenaRadnogSata:parseFloat(podaci.get('cijenaRadnogSata')),
-            koeficijentRadnogMjesta:parseFloat(podaci.get('koeficijentRadnogMjesta')),
+            cijenaRadnogSata: parseFloat(podaci.get('cijenaradnogsata')),
+            koeficijentRadnogMjesta: parseFloat(podaci.get('koeficijentradnogmjesta')),
             osnovniOsobniOdbitak: parseFloat(podaci.get('osnovniosobniodbitak')),
-          };
+        };
 
-          //console.log(JSON.stringify(smjer));
-          promjeniRadnika(radnik);
+        //console.log(JSON.stringify(smjer));
+        promjeniRadnika(radnik);
     }
 
-    
 
-    
+
+
     return (
         <Container>
             <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="ime">
                     <Form.Label>Ime</Form.Label>
-                    <Form.Control 
+                    <Form.Control
                         type="text"
                         defaultValue={radnik.ime}
-                        name="ime"/>
+                        name="ime" />
                 </Form.Group>
                 <Form.Group controlId="prezime">
                     <Form.Label>Prezime</Form.Label>
-                    <Form.Control 
+                    <Form.Control
                         type="text"
                         defaultValue={radnik.prezime}
-                        name="prezime"/>
+                        name="prezime" />
                 </Form.Group>
                 <Form.Group controlId="oib">
                     <Form.Label>OiB</Form.Label>
-                    <Form.Control 
+                    <Form.Control
                         type="text"
                         defaultValue={radnik.oib}
-                        name="oib"/>
+                        name="oib" />
                 </Form.Group>
                 <Form.Group controlId="datumzaposlenja">
                     <Form.Label>DatumZaposlenja</Form.Label>
-                    <Form.Control 
+                    <Form.Control
                         type="text"
                         defaultValue={radnik.datumzaposlenja}
-                        name="datumzaposlenja"/>
+                        name="datumzaposlenja" />
                 </Form.Group>
                 <Form.Group controlId="iban">
                     <Form.Label>Iban</Form.Label>
-                    <Form.Control 
+                    <Form.Control
                         type="text"
                         defaultValue={radnik.iban}
-                        name="iban"/>
+                        name="iban" />
                 </Form.Group>
-                <Form.Group controlId="cijenaRadnogSata">
+                <Form.Group controlId="cijenaradnogsata">
                     <Form.Label>Cijena radnog sata</Form.Label>
-                    <Form.Control 
-                        type="text"
-                        defaultValue={radnik.cijenaRadnogSata}
-                        name="cijenaRadnogSata"
-                        placeholder='Cijena radnog sata'
-                    />
+                    <div className="input-group">
+                        <Form.Control
+                            type="text"
+                            defaultValue={radnik.cijenaradnogsata}
+                            name="cijenaRadnogSata"
+                            placeholder='Cijena radnog sata'
+                        />
+                        <span className="input-group-text">€</span>
+                    </div>
                 </Form.Group>
                 <Form.Group controlId="koeficijentRadnogMjesta">
                     <Form.Label>Koeficijent radnog mjesta </Form.Label>
-                    <Form.Control 
+                    <Form.Control
                         type="text"
-                        defaultValue={radnik.koeficijentRadnogMjesta}
+                        defaultValue={radnik.koeficijentradnogmjesta}
                         name="koeficijentRadnogMjesta"
                         placeholder='Koeficijent radnog mjesta'
-                        
+
                     />
                 </Form.Group>
-            <Row className="akcije">
-                <Col>
-                    <Link 
-                        className="btn btn-danger"
-                        to={RoutesNames.RADNICI_PREGLED}>
-                        <RiArrowGoBackFill size={15} />    
-                    Odustani
-                    </Link>
-                </Col>
-            </Row>
+
+                <Form.Group controlId="osnovniosobniodbitak">
+                    <Form.Label>Osobni osobni odbitak </Form.Label>
+                    <div className="input-group">
+                    <Form.Control
+                        type="text"
+                        defaultValue={radnik.osnovniosobniodbitak}
+                        name="osnovniosobniodbitak"
+                        placeholder='Osobni osobni odbitak'
+
+                    />
+                    <span className="input-group-text">€</span>
+                    </div>
+                </Form.Group>
+                <Row className="akcije">
+                    <Col>
+                        <Link
+                            className="btn btn-danger"
+                            to={RoutesNames.RADNICI_PREGLED}>
+                            <RiArrowGoBackFill size={15} />
+                            Odustani
+                        </Link>
+                    </Col>
+                </Row>
             </Form>
-         </Container>
+        </Container>
 
     );
 }
--- Frontend/src/pages/radnici/RadniciPomjeni.jsx
+++ Frontend/src/pages/radnici/RadniciPomjeni.jsx
@@ -7,146 +7,152 @@ import RadnikService from "../../services/RadnikService";
 import { useEffect, useState } from "react";
 
 
-export default function RadniciPomjeni (){
+export default function RadniciPomjeni() {
     const navigate = useNavigate();
     const routeParams = useParams();
-    const [radnik,setRadnici] = useState({});
+    const [radnik, setRadnici] = useState({});
 
-    async function dohvatiRadnike(){
+    async function dohvatiRadnike() {
         await RadnikService.getBySifra(routeParams.sifra)
-        .then((res)=>{
-            setRadnici(res.data)
-        })
-        .catch((e)=>{
-            alert(e.poruka);
-        });
+            .then((res) => {
+                setRadnici(res.data)
+            })
+            .catch((e) => {
+                alert(e.poruka);
+            });
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         //console.log("useEffect")
         dohvatiRadnike();
-    },[]);
+    }, []);
 
-    async function promjeniRadnika(radnik){
-        const odgovor = await RadnikService.promjeni(routeParams.sifra,radnik);
-        if(odgovor.ok){
-          navigate(RoutesNames.RADNICI_PREGLED);
-        }else{
-          console.log(odgovor);
-          alert(odgovor.poruka);
+    async function promjeniRadnika(radnik) {
+        const odgovor = await RadnikService.promjeni(routeParams.sifra, radnik);
+        if (odgovor.ok) {
+            navigate(RoutesNames.RADNICI_PREGLED);
+        } else {
+            console.log(odgovor);
+            alert(odgovor.poruka);
         }
     }
 
-    function handleSubmit(e){
+    function handleSubmit(e) {
         e.preventDefault();
         const podaci = new FormData(e.target);
 
-        const radnik = 
+        const radnik =
         {
             ime: podaci.get('ime'),
             prezime: podaci.get('prezime'),
             oib: podaci.get('oib'),
             datumZaposlenja: podaci.get('datumzaposlenja'),
             iban: podaci.get('iban'),
-            cijenaRadnogSata:parseFloat(podaci.get('cijenaRadnogSata')),
-            koeficijentRadnogMjesta:parseFloat(podaci.get('koeficijentRadnogMjesta')),
+            cijenaRadnogSata: parseFloat(podaci.get('cijenaRadnogSata')),
+            koeficijentRadnogMjesta: parseFloat(podaci.get('koeficijentRadnogMjesta')),
             osnovniOsobniOdbitak: parseFloat(podaci.get('osnovniosobniodbitak')),
-          };
+        };
 
-          //console.log(JSON.stringify(smjer));
-          promjeniRadnika(radnik);
+        //console.log(JSON.stringify(smjer));
+        promjeniRadnika(radnik);
     }
 
-    
 
-    
+
+
     return (
         <Container>
             <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="ime">
                     <Form.Label>Ime</Form.Label>
-                    <Form.Control 
+                    <Form.Control
                         type="text"
                         defaultValue={radnik.ime}
-                        name="ime"/>
+                        name="ime" />
                 </Form.Group>
                 <Form.Group controlId="prezime">
                     <Form.Label>Prezime</Form.Label>
-                    <Form.Control 
+                    <Form.Control
                         type="text"
                         defaultValue={radnik.prezime}
-                        name="prezime"/>
+                        name="prezime" />
                 </Form.Group>
                 <Form.Group controlId="oib">
                     <Form.Label>OiB</Form.Label>
-                    <Form.Control 
+                    <Form.Control
                         type="text"
                         defaultValue={radnik.oib}
-                        name="oib"/>
+                        name="oib" />
                 </Form.Group>
                 <Form.Group controlId="datumzaposlenja">
                     <Form.Label>DatumZaposlenja</Form.Label>
-                    <Form.Control 
+                    <Form.Control
                         type="text"
                         defaultValue={radnik.datumzaposlenja}
-                        name="datumzaposlenja"/>
+                        name="datumzaposlenja" />
                 </Form.Group>
                 <Form.Group controlId="iban">
                     <Form.Label>Iban</Form.Label>
-                    <Form.Control 
+                    <Form.Control
                         type="text"
                         defaultValue={radnik.iban}
-                        name="iban"/>
+                        name="iban" />
                 </Form.Group>
                 <Form.Group controlId="cijenaRadnogSata">
                     <Form.Label>Cijena radnog sata</Form.Label>
-                    <Form.Control 
-                        type="text"
-                        defaultValue={radnik.cijenaradnogsata}
-                        name="cijenaRadnogSata"
-                        placeholder='Cijena radnog sata'
-                    />
+                    <div className="input-group">
+                        <Form.Control
+                            type="text"
+                            defaultValue={radnik.cijenaradnogsata}
+                            name="cijenaRadnogSata"
+                            placeholder='Cijena radnog sata'
+                        />
+                        <span className="input-group-text">€</span>
+                    </div>
                 </Form.Group>
                 <Form.Group controlId="koeficijentRadnogMjesta">
                     <Form.Label>Koeficijent radnog mjesta </Form.Label>
-                    <Form.Control 
+                    <Form.Control
                         type="text"
                         defaultValue={radnik.koeficijentradnogmjesta}
                         name="koeficijentRadnogMjesta"
                         placeholder='Koeficijent radnog mjesta'
-                        
+
                     />
                 </Form.Group>
                 <Form.Group controlId="osnovniosobniodbitak">
                     <Form.Label>Osobni osobni odbitak </Form.Label>
-                    <Form.Control 
-                        type="text"
-                        defaultValue={radnik.osnovniosobniodbitak}
-                        name="osnovniosobniodbitak"
-                        placeholder='Osobni osobni odbitak'
-                        
-                    />
+                    <div className="input-group">
+                        <Form.Control
+                            type="text"
+                            defaultValue={radnik.osnovniosobniodbitak}
+                            name="osnovniosobniodbitak"
+                            placeholder='Osobni osobni odbitak'
+
+                        />
+                        <span className="input-group-text">€</span>
+                    </div>
                 </Form.Group>
-            <Row className="akcije">
-                <Col>
-                    <Link 
-                        className="btn btn-danger"
-                        to={RoutesNames.RADNICI_PREGLED}>
-                        <RiArrowGoBackFill size={15} />    
-                    Odustani
-                    </Link>
-                </Col>
-                <Col>
-                    <Button
-                        variant="primary"
-                        type="submit">
-                        <RiArrowGoForwardFill size ={15} />
-                    Promjeni radnika 
-                    </Button>
-                </Col>
-            </Row>
+                <Row className="akcije">
+                    <Col>
+                        <Link
+                            className="btn btn-danger"
+                            to={RoutesNames.RADNICI_PREGLED}>
+                            <RiArrowGoBackFill size={15} />
+                            Odustani
+                        </Link>
+                    </Col>
+                    <Col>
+                        <Button
+                            variant="primary"
+                            type="submit">
+                            <RiArrowGoForwardFill size={15} />
+                            Promjeni radnika
+                        </Button>
+                    </Col>
+                </Row>
             </Form>
-         </Container>
+        </Container>
 
     );
 }
