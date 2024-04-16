a1999ae221a3563122fb54286f434fdd44a8ef39
Ivan Leninger <ileninger@live.com>
2024-04-16 17:53:57 +0200

    Proradio LOG IN

--- Frontend/src/services/authService.js
+++ Frontend/src/services/authService.js
@@ -1,7 +1,8 @@
 import { httpService, obradiGresku, obradiUspjeh } from './httpService';
-
+import axios from "axios";
 export async function logInService(userData) {
   return await httpService
     .post('/Autorizacija/token', userData)
-    .then((res)=>{return obradiUspjeh(res);}).catch((e)=>{ return { greska: true , podaci: [{svojstvo: 'Autorizacija', poruka: e.response.data}]}});
+    .then((res)=>{return  {ok: true, podaci: res};;})
+    .catch((e)=>{ return { greska: true , podaci: [{svojstvo: 'Autorizacija', poruka: e.response.data}]}});
 }
