5032849fc54e1459eb21e32c336082a232ecef67
Ivan Leninger <ileninger@live.com>
2024-04-16 20:58:31 +0200

    Dodani  OiB validator

--- CSHARP/ZavrsniRad/WebApi_ZavrsniRad/Models/DTO.cs
+++ CSHARP/ZavrsniRad/WebApi_ZavrsniRad/Models/DTO.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using EdunovaAPP.Validations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using WebApi_ZavrsniRad.Models;
 
@@ -6,13 +7,24 @@ namespace WebApi_ZavrsniRad.Models
 {
     public record RadnikDTORead(int sifra, string ime, string prezime,
         string oib, DateTime datumzaposlenja, string iban, decimal cijenaradnogsata, decimal koeficijentradnogmjesta, decimal osnovniosobniodbitak);
-    public record RadnikDTOInsertUpdate(string ime, string prezime,
-    string oib, DateTime datumzaposlenja, string iban, decimal cijenaradnogsata, decimal koeficijentradnogmjesta, decimal osnovniosobniodbitak);
+    public record RadnikDTOInsertUpdate(
+        string ime, 
+        string prezime,
+        [OibValidator]
+        string oib, 
+        DateTime datumzaposlenja, 
+        string iban, decimal cijenaradnogsata, 
+        decimal koeficijentradnogmjesta, 
+        decimal osnovniosobniodbitak);
 
     public record PodaciZaObracuneDTORead(int sifra, string naziv, decimal postotakzaprvimirovinskistup,
         decimal postotakzadrugimirovinskistup, decimal stopaporezanadohodak);
-    public record PodaciZaObracuneDTOInsertUpdate(string naziv, decimal postotakzaprvimirovinskistup,
-    decimal postotakzadrugimirovinskistup, decimal stopaporezanadohodak);
+    public record PodaciZaObracuneDTOInsertUpdate(string naziv,
+        [Range(0, 99, ErrorMessage = "Vrijednost {0} mora biti između {1} i {2}")]
+        decimal postotakzaprvimirovinskistup,
+        [Range(0, 99, ErrorMessage = "Vrijednost {0} mora biti između {1} i {2}")]
+        decimal postotakzadrugimirovinskistup, 
+        decimal stopaporezanadohodak);
 
     public record PlacaDTORead(int sifra, string nazivplace, int brojradnihsati, DateTime datumpocetkaplace, DateTime datumkrajaplace);
 
---
+++ CSHARP/ZavrsniRad/WebApi_ZavrsniRad/Validations/OibValidator.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace EdunovaAPP.Validations
+{
+    public class OibValidator : ValidationAttribute
+    {
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return ValidationResult.Success;
+            }
+            var oib = (string)value;
+            if (oib.Length==0 )
+            {
+                return ValidationResult.Success;
+            }
+            return IsValidOIB(oib) ? ValidationResult.Success : new ValidationResult("OIB nije formalno valjan"); ;
+        }
+
+        // https://github.com/domagojpa/oib-validation/blob/main/CSharp/oib-validation.cs
+        public static bool IsValidOIB(string oib)
+        {
+            if (string.IsNullOrEmpty(oib) || !Regex.IsMatch(oib, "^[0-9]{11}$"))
+                return false;
+
+            var oibSpan = oib.AsSpan();
+            var a = 10;
+            for (var i = 0; i < 10; i++)
+            {
+                a += int.Parse(oibSpan.Slice(i, 1));
+                a %= 10;
+
+                if (a == 0)
+                    a = 10;
+
+                a *= 2;
+                a %= 11;
+            }
+
+            var kontrolni = 11 - a;
+
+            if (kontrolni == 10)
+                kontrolni = 0;
+
+            return kontrolni == int.Parse(oibSpan.Slice(10, 1));
+        }
+
+    }
+}
